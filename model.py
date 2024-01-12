cb1932384507816581545842b6a14127e9fc8700
Dhrubaraj Roy <dhrubarajroy123@gmail.com>
2024-01-12 11:14:56 +0530

    file structure done

--- model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-#Class which describes Bank Notes measurements
-class mymodel(BaseModel):
-    Postal_Code: int
-    Model_Year: int
-    Legislative_District: int
-    
